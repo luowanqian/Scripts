94e298817c276a51e074e6e3c8edbd8421bea481
luowanqian <luowanqian94@gmail.com>
2019-02-05 22:30:10 +0800

    first commit

---
+++ ssh/server.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+#======================
+# Custom Variable
+#======================
+
+SSH_PORT=22
+SSH_KEY_FILE="id_rsa"
+REMOTE_SSH_PORT=22
+REMOTE_USER=root
+REMOTE_IP=0.0.0.0
+REMOTE_PORT=8888
+
+#=============================
+
+# generate ssh key file
+ssh-keygen -t rsa -b 4096 -f ${SSH_KEY_FILE} -q
+
+# copy ssh key file to remote server
+ssh-copy-id -p ${REMOTE_SSH_PORT} -i ~/.ssh/${SSH_KEY_FILE} ${REMOTE_USER}@${REMOTE_IP}
+
+# autossh
+autossh -M 0 -fCNR ${REMOTE_PORT}:localhost:${SSH_PORT} -p ${REMOTE_SSH_PORT} ${REMOTE_USER}@${REMOTE_IP}
