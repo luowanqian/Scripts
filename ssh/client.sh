d961f3be599eb1f6647ae3e4507484d8ccfe3089
luowanqian <luowanqian94@gmail.com>
2019-02-05 23:10:42 +0800

    update ssh

--- ssh/client.sh
+++ ssh/client.sh
@@ -6,6 +6,7 @@
 #======================
 
 PORT=8888
+SSH_PORT=4084
 REMOTE_PORT=8889
 SSH_KEY_FILE="id_rsa"
 
@@ -15,4 +16,4 @@ SSH_KEY_FILE="id_rsa"
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/${SSH_KEY_FILE} -C "localhost" -q
 
 # autossh
-autossh -M 0 -fCNL *:${REMOTE_PORT}:localhost:${PORT} localhost
+autossh -M 0 -fCNL *:${REMOTE_PORT}:localhost:${PORT} -f ~/.ssh/${SSH_KEY_FILE} -p ${SSH_PORT} localhost
